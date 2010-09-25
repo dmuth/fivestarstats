69a99d5f84ed21bdde6959833d186329f5e6d382
Douglas Muth <doug.muth@gmail.com>
2010-09-25 16:37:31 -0400

    Refactored code into fivestarstats_get_user_stats().

--- get_user_stats.inc.php
+++ get_user_stats.inc.php
@@ -28,37 +28,7 @@ function fivestarstats_get_user_stats($num) {
 
 		$retval[$uid] = fivestarstats_get_user_stats_avg_rating($uid);
 
-		//
-		// Get a breakdown of ratings for each user.
-		//
-		$query = ""
-			. "SELECT value, sum(cnt) AS cnt FROM "
-			. "("
-			. "SELECT 'node' AS type, value, COUNT(*) AS cnt "
-			. "FROM votingapi_vote "
-			. "WHERE "
-			. "value_type='percent' "
-			. "AND content_type='node' AND content_id IN "
-				. "(SELECT nid FROM node WHERE uid=%d) "
-			. "GROUP BY value "
-			. "UNION "
-			. "SELECT 'comment' AS type, value, COUNT(*) AS cnt "
-			. "FROM votingapi_vote "
-			. "WHERE "
-			. "value_type='percent' "
-			. "AND content_type='comment' AND content_id IN "
-				. "(SELECT cid FROM comments WHERE uid=%d) "
-			. "GROUP BY value "
-			. ") tbl1 "
-			. "GROUP BY value "
-			;
-		$query_args = array($uid, $uid);
-		$cursor = db_query($query, $query_args);
-		$retval[$uid]["num_stars"] = array();
-		while ($row = db_fetch_array($cursor)) {
-			$num_stars = $row["value"] / 20;
-			$retval[$uid]["num_stars"][$num_stars] = $row["cnt"];
-		}
+		$retval[$uid]["num_stars"] = fivestarstats_get_user_stats_ratings($uid);
 
 	}
 
@@ -140,6 +110,58 @@ function fivestarstats_get_user_stats_avg_rating($uid) {
 	$retval["num_votes"] = $row["cnt"];
 	$retval["total"] = $row["total"];
 	$retval["average"] = $row["total"] / $row["cnt"] / 20;
+
+	return($retval);
+
+} // End of fivestarstats_get_user_stats_avg_rating()
+
+
+/**
+* Get a breakdown of ratings by star number for a specific user.
+*
+* @param integer $uid the User ID we want an average rating for.
+*
+* @return array Associative array of data.
+*/
+function fivestarstats_get_user_stats_ratings($uid) {
+
+	$retval = array();
+
+	//
+	// Get a breakdown of ratings for each user.
+	//
+	$query = ""
+		. "SELECT value, sum(cnt) AS cnt FROM "
+		. "("
+		. "SELECT 'node' AS type, value, COUNT(*) AS cnt "
+		. "FROM votingapi_vote "
+		. "WHERE "
+		. "value_type='percent' "
+		. "AND content_type='node' AND content_id IN "
+			. "(SELECT nid FROM node WHERE uid=%d) "
+		. "GROUP BY value "
+		. "UNION "
+		. "SELECT 'comment' AS type, value, COUNT(*) AS cnt "
+		. "FROM votingapi_vote "
+		. "WHERE "
+		. "value_type='percent' "
+		. "AND content_type='comment' AND content_id IN "
+			. "(SELECT cid FROM comments WHERE uid=%d) "
+		. "GROUP BY value "
+		. ") tbl1 "
+		. "GROUP BY value "
+		;
+	$query_args = array($uid, $uid);
+	$cursor = db_query($query, $query_args);
+	while ($row = db_fetch_array($cursor)) {
+		$num_stars = $row["value"] / 20;
+		$retval[$num_stars] = $row["cnt"];
+	}
+
+	//
+	// Put the highest ratings first.
+	//
+	krsort($retval);
 
 	return($retval);
 
