307f7484667711e06581ca53327e07efd6023237
Douglas Muth <doug.muth@gmail.com>
2010-09-25 16:29:06 -0400

    Refactored code into fivestarstats_get_user_stats_top_posters().

--- get_user_stats.inc.php
+++ get_user_stats.inc.php
@@ -9,36 +9,18 @@
 /**
 * Get statistics on users.
 *
-* @param integer $num_users The number of "top" users to query.
+* @param integer $num The number of "top" users to query.
 *
 * @return array Associative array of data where the key is the user ID.
 */
-function fivestarstats_get_user_stats($num_users) {
+function fivestarstats_get_user_stats($num) {
 
 	$retval = array();
 
 	//
 	// Get top posters. Both nodes AND comments.
 	//
-	$query = "SELECT tbl1.uid, sum(cnt) AS cnt, users.name FROM "
-		. "(SELECT uid, count(*) AS cnt FROM comments GROUP BY uid "
-			. "UNION SELECT uid, count(*) AS cnt FROM node GROUP BY uid)"
-			. " tbl1 "
-		. "JOIN users ON tbl1.uid = users.uid GROUP BY uid ORDER BY cnt DESC "
-		. "LIMIT $num_users"
-		;
-	$cursor = db_query($query);
-	
-	while ($row = db_fetch_array($cursor)) {
-
-		$uid = $row["uid"];
-		$user = array();
-		$user["num_posts"] = $row["cnt"];
-		$user["name"] = $row["name"];
-
-		$retval[$uid] = $user;
-
-	}
+	$retval = fivestarstats_get_user_stats_top_posters($num);
 
 	foreach ($retval as $key => $value) {
 
@@ -75,7 +57,7 @@ function fivestarstats_get_user_stats($num_users) {
 		// Get a breakdown of ratings for each user.
 		//
 		$query = ""
-			. "SELECT value /20 AS stars, sum(cnt) AS cnt FROM "
+			. "SELECT value, sum(cnt) AS cnt FROM "
 			. "("
 			. "SELECT 'node' AS type, value, COUNT(*) AS cnt "
 			. "FROM votingapi_vote "
@@ -99,7 +81,7 @@ function fivestarstats_get_user_stats($num_users) {
 		$cursor = db_query($query, $query_args);
 		$retval[$uid]["num_stars"] = array();
 		while ($row = db_fetch_array($cursor)) {
-			$num_stars = $row["stars"];
+			$num_stars = $row["value"] / 20;
 			$retval[$uid]["num_stars"][$num_stars] = $row["cnt"];
 		}
 
@@ -110,4 +92,37 @@ function fivestarstats_get_user_stats($num_users) {
 } // End of fivestarstats_get_user_stats()
 
 
+/**
+* Get top posters by number of posts/comments.
+*
+* @param integer $num The number of "top" users to query.
+*
+* @return array Associative array of data where the key is the user ID.
+*/
+function fivestarstats_get_user_stats_top_posters($num) {
 
+	$retval = array();
+
+	$query = "SELECT tbl1.uid, sum(cnt) AS cnt, users.name FROM "
+		. "(SELECT uid, count(*) AS cnt FROM comments GROUP BY uid "
+			. "UNION SELECT uid, count(*) AS cnt FROM node GROUP BY uid)"
+			. " tbl1 "
+		. "JOIN users ON tbl1.uid = users.uid GROUP BY uid ORDER BY cnt DESC "
+		. "LIMIT $num"
+		;
+	$cursor = db_query($query);
+	
+	while ($row = db_fetch_array($cursor)) {
+
+		$uid = $row["uid"];
+		$user = array();
+		$user["num_posts"] = $row["cnt"];
+		$user["name"] = $row["name"];
+
+		$retval[$uid] = $user;
+
+	}
+
+	return($retval);
+
+} // End of fivestarstats_get_user_stats_top_posters()
