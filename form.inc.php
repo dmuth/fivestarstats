24f2a29e277c9aa7c63cec5345a9c429e8061c58
Douglas Muth <doug.muth@gmail.com>
2010-09-25 21:25:31 -0400

    Added UI for top IPs.

--- form.inc.php
+++ form.inc.php
@@ -19,7 +19,8 @@ function fivestarstats_form($form_data, $data) {
 
 	$retval = array();
 
-	$retval["total_votes"] = fivestarstats_form_total_votes($data);
+	$retval["total_votes"] = fivestarstats_form_total_votes($data["total"]);
+	$retval["ips"] = fivestarstats_form_ips($data["ips"]);
 
 	return($retval);
 
@@ -35,8 +36,6 @@ function fivestarstats_form($form_data, $data) {
 */
 function fivestarstats_form_total_votes($data) {
 
-	$retval = array();
-
 	$retval = array(
 		"#type" => "fieldset",
 		"#collapsible" => true,
@@ -47,13 +46,87 @@ function fivestarstats_form_total_votes($data) {
 		"#type" => "item",
 		"#value" => t("%num_votes from %num_ips unique IPs "
 			. "with average of %num_stars stars per vote.",
-			array("%num_votes" => $data["total"]["num_votes"],
-				"%num_ips" => $data["total"]["num_ips"],
-				"%num_stars" => $data["total"]["average_stars"])
+			array("%num_votes" => $data["num_votes"],
+				"%num_ips" => $data["num_ips"],
+				"%num_stars" => $data["average_stars"])
 			)
 		);
 
 	return($retval);
 
 } // End of fivestarstats_form_total_values()
+
+
+/**
+* Create form elements for top IP addresses.
+*/
+function fivestarstats_form_ips($data) {
+
+	$retval = array(
+		"#type" => "fieldset",
+		"#collapsible" => true,
+		"#title" => t("IP Activity"),
+		);
+
+	//
+	// Top voters
+	//
+	$header = array(t("IP"), t("Number of votes"));
+	
+	$rows = array();
+	foreach ($data["top_voters"] as $key => $value) {
+		$row = array($key, $value . t(" votes"));
+		$rows[] = $row;
+	}
+
+	$content = theme("table", $header, $rows);
+
+	$retval["top_voters"] = array(
+		"#type" => "fieldset",
+		"#title" => t("Top Voters"),
+		"#description" => $content,
+		);
+
+	//
+	// Top 1-star voters
+	//
+	$rows = array();
+	foreach ($data["top_1_star_voters"] as $key => $value) {
+		$row = array($key, $value . t(" votes"));
+		$rows[] = $row;
+	}
+
+	$content = theme("table", $header, $rows);
+
+	$retval["top_1_star_voters"] = array(
+		"#type" => "fieldset",
+		"#title" => t("Top 1-star Voters"),
+		"#description" => $content,
+		);
+
+	//
+	// Lowest vote average
+	//
+	$header = array(t("IP"), t("Number of Votes"), t("Average Vote"));
+	$rows = array();
+	foreach ($data["lowest_vote_average"] as $key => $value) {
+		$row = array($key, $value["cnt"] . t(" votes"), 
+			sprintf("%.2f", $value["avg"]) . t(" stars")
+			);
+		$rows[] = $row;
+	}
+
+	$content = theme("table", $header, $rows);
+
+	$retval["lowest_vote_average"] = array(
+		"#type" => "fieldset",
+		"#title" => t("Lowest Vote Average"),
+		"#description" => $content,
+		);
+
+	return($retval);
+
+} // End of fivestarstats_form_ips()
+
+
 
