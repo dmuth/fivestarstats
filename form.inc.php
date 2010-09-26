fdda76845738c9c1866c424939f72514fbba88bc
Douglas Muth <doug.muth@gmail.com>
2010-09-25 22:06:01 -0400

    Added UI for users.

--- form.inc.php
+++ form.inc.php
@@ -21,6 +21,7 @@ function fivestarstats_form($form_data, $data) {
 
 	$retval["total_votes"] = fivestarstats_form_total_votes($data["total"]);
 	$retval["ips"] = fivestarstats_form_ips($data["ips"]);
+	$retval["users"] = fivestarstats_form_users($data["users"]);
 
 	return($retval);
 
@@ -75,7 +76,9 @@ function fivestarstats_form_ips($data) {
 	
 	$rows = array();
 	foreach ($data["top_voters"] as $key => $value) {
-		$row = array($key, $value . t(" votes"));
+		$row = array($key, 
+			array("data" => $value . t(" votes"), "align" => "right"),
+			);
 		$rows[] = $row;
 	}
 
@@ -92,7 +95,9 @@ function fivestarstats_form_ips($data) {
 	//
 	$rows = array();
 	foreach ($data["top_1_star_voters"] as $key => $value) {
-		$row = array($key, $value . t(" votes"));
+		$row = array($key, 
+			array("data" => $value . t(" votes"), "align" => "right"),
+			);
 		$rows[] = $row;
 	}
 
@@ -110,8 +115,9 @@ function fivestarstats_form_ips($data) {
 	$header = array(t("IP"), t("Number of Votes"), t("Average Vote"));
 	$rows = array();
 	foreach ($data["lowest_vote_average"] as $key => $value) {
-		$row = array($key, $value["cnt"] . t(" votes"), 
-			sprintf("%.2f", $value["avg"]) . t(" stars")
+		$row = array($key, 
+			array("data" => $value["cnt"] . t(" votes"), "align" => "right"),
+			array("data" => sprintf("%.2f", $value["avg"]) . t(" stars"), "align" => "right"),
 			);
 		$rows[] = $row;
 	}
@@ -129,4 +135,112 @@ function fivestarstats_form_ips($data) {
 } // End of fivestarstats_form_ips()
 
 
+/**
+* Create form elements with user info.
+*/
+function fivestarstats_form_users($data) {
 
+	$retval = array();
+
+	$retval = array(
+		"#type" => "fieldset",
+		"#collapsible" => true,
+		"#title" => t("User Activity"),
+		);
+
+	//
+	// Top-rated users
+	//
+	$header = array(t("Username"), t("# of times voted on"), t("Average Vote"));
+	$rows = array();
+	foreach ($data["top_rated"] as $key => $value) {
+		$row = array($value["name"], 
+			array("data" => $value["num_votes"], "align" => "right"),
+			array("data" => sprintf("%.2f", $value["average"]) . t(" stars"),
+				"align" => "right"),
+			);
+		$rows[] = $row;
+	}
+
+	$content = theme("table", $header, $rows);
+
+	$retval["top_rated"] = array(
+		"#type" => "fieldset",
+		"#title" => t("Top-rated Users"),
+		"#description" => $content,
+		);
+	
+	//
+	// Bottom-rated users
+	//
+	$rows = array();
+	foreach ($data["bottom_rated"] as $key => $value) {
+		$row = array($value["name"], 
+			array("data" => $value["num_votes"], "align" => "right"),
+			array("data" => sprintf("%.2f", $value["average"]) . t(" stars"),
+				"align" => "right"),
+			);
+		$rows[] = $row;
+		
+	}
+
+	$content = theme("table", $header, $rows);
+
+	$retval["bottom_rated"] = array(
+		"#type" => "fieldset",
+		"#title" => t("Bottom-rated Users"),
+		"#description" => $content,
+		);
+
+	//
+	// Most 1-star votes received.
+	//
+	$header = array(t("Username"), t("# of 1-star votes received"));
+
+	$rows = array();
+	foreach ($data["most_1_star_votes"] as $key => $value) {
+		$row = array($value["name"], 
+			array("data" => $value["cnt"] . t(" votes"),
+				"align" => "right")
+			);
+		$rows[] = $row;
+	}
+
+	$content = theme("table", $header, $rows);
+
+	$retval["most_1_star_votes"] = array(
+		"#type" => "fieldset",
+		"#title" => t("Most 1-star votes received"),
+		"#description" => $content,
+		);
+
+
+	//
+	// Top Posters
+	//
+	$header = array(t("Username"), t("# posts"), t("# votes received"), 
+		t("average rating"));
+	$rows = array();
+	foreach ($data["top_posters"] as $key => $value) {
+		$row = array($value["name"], 
+			array("data" => $value["num_posts"] . t(" posts"), 
+				"align" => "right"),
+			array("data" => $value["num_votes"] . t(" votes"), 
+				"align" => "right"),
+			array("data" => sprintf("%.2f stars", $value["average"]), 
+				"align" => "right"),
+			);
+		$rows[] = $row;
+	}
+
+	$content = theme("table", $header, $rows);
+
+	$retval["top_posters"] = array(
+		"#type" => "fieldset",
+		"#title" => t("Top posters by # posts/comments"),
+		"#description" => $content,
+		);
+
+	return($retval);
+
+} // End of fivestarstats_form_users()
